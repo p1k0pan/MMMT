8bd067920790cca31b49802cbcfe0961a64b691f
p1k0pan <645758634@qq.com>
2024-12-21 16:06:55 +0100

    initial; mcd without special token

---
+++ llava_icd/model/language_model/llava_llama.py
@@ -0,0 +1,266 @@
+#    Copyright 2023 Haotian Liu
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+
+from typing import List, Optional, Tuple, Union
+
+import torch
+import torch.nn as nn
+
+from transformers import AutoConfig, AutoModelForCausalLM, \
+                         LlamaConfig
+
+from .modeling_llama_cd import LlamaModel, LlamaForCausalLM
+
+# from transformers import AutoConfig, AutoModelForCausalLM, \
+#                          LlamaConfig, LlamaModel, LlamaForCausalLM
+
+from transformers.modeling_outputs import CausalLMOutputWithPast
+from transformers.generation.utils import GenerateOutput
+
+from ..llava_arch import LlavaMetaModel, LlavaMetaForCausalLM
+import sys
+
+
+
+class LlavaConfig(LlamaConfig):
+    model_type = "llava_llama"
+
+
+class LlavaLlamaModel(LlavaMetaModel, LlamaModel):
+    config_class = LlavaConfig
+
+    def __init__(self, config: LlamaConfig):
+        super(LlavaLlamaModel, self).__init__(config)
+
+
+class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
+    config_class = LlavaConfig
+
+    def __init__(self, config):
+        super(LlamaForCausalLM, self).__init__(config)
+        self.model = LlavaLlamaModel(config)
+        self.pretraining_tp = config.pretraining_tp
+        self.vocab_size = config.vocab_size
+        self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def get_model(self):
+        return self.model
+
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        images: Optional[torch.FloatTensor] = None,
+        image_sizes: Optional[List[List[int]]] = None,
+        return_dict: Optional[bool] = None,
+        inputs_embeds_icd: Optional[torch.FloatTensor] = None,
+        inputs_embeds_vcd: Optional[torch.FloatTensor] = None,
+        inputs_embeds_lcd: Optional[torch.FloatTensor] = None,
+        sd_am=None,
+        image_position=None
+    ) -> Union[Tuple, CausalLMOutputWithPast]:
+
+        if inputs_embeds is None:
+            (
+                input_ids,
+                position_ids,
+                attention_mask,
+                past_key_values,
+                inputs_embeds,
+                labels
+            ) = self.prepare_inputs_labels_for_multimodal(
+                input_ids,
+                position_ids,
+                attention_mask,
+                past_key_values,
+                labels,
+                images,
+                image_sizes
+            )
+        return super().forward(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            labels=labels,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+            image_position=image_position,
+        )
+
+    @torch.no_grad()
+    def generate(
+        self,
+        inputs: Optional[torch.Tensor] = None,
+        images: Optional[torch.Tensor] = None,
+        image_sizes: Optional[torch.Tensor] = None,
+        input_ids_icd: Optional[torch.Tensor] = None,
+        images_cd: Optional[torch.Tensor] = None,
+        input_ids_lcd: Optional[torch.Tensor] = None,
+        sid: Optional[bool] = False,
+        img_topk: Optional[torch.Tensor] = None,
+        **kwargs,
+    ) -> Union[GenerateOutput, torch.LongTensor]:
+        position_ids = kwargs.pop("position_ids", None)
+        attention_mask = kwargs.pop("attention_mask", None)
+        if "inputs_embeds" in kwargs:
+            raise NotImplementedError("`inputs_embeds` is not supported")
+
+        if input_ids_icd is not None and input_ids_lcd is not None:
+            max_prompt_len = max(inputs.shape[1]-1, input_ids_icd.shape[1]-1, input_ids_lcd.shape[1]-1)
+        elif input_ids_icd is not None and input_ids_lcd is None:
+            max_prompt_len = max(inputs.shape[1]-1, input_ids_icd.shape[1]-1)
+        else:
+            max_prompt_len = inputs.shape[1]-1
+        inputs_vcd = inputs.clone()
+        if images is not None:
+            (
+                inputs,
+                position_ids,
+                attention_mask,
+                _,
+                inputs_embeds,
+                _,
+                image_position
+            ) = self.prepare_inputs_labels_for_multimodal(
+            # ) = self.prepare_inputs_labels_for_text(
+                inputs,
+                position_ids,
+                attention_mask,
+                None,
+                None,
+                images,
+                image_sizes=image_sizes,
+                max_prompt_len=max_prompt_len
+            )
+        else:
+            inputs_embeds = self.get_model().embed_tokens(inputs)
+        
+        if input_ids_icd is not None: 
+            (
+                input_ids_icd,
+                position_ids,
+                _,
+                _,
+                inputs_embeds_icd,
+                _,
+                _
+            ) = self.prepare_inputs_labels_for_multimodal(
+            # ) = self.prepare_inputs_labels_for_text(
+                input_ids_icd,
+                position_ids,
+                None,
+                None,
+                None,
+                images,
+                image_sizes=image_sizes,
+                max_prompt_len=max_prompt_len
+            ) 
+        else:
+            inputs_embeds_icd = None
+        if images_cd is not None: 
+            (
+                inputs,
+                position_ids,
+                _,
+                _,
+                inputs_embeds_vcd,
+                _,
+                _
+            ) = self.prepare_inputs_labels_for_multimodal(
+                inputs_vcd,
+                position_ids,
+                None,
+                None,
+                None,
+                images_cd,
+                image_sizes=image_sizes,
+                max_prompt_len=None
+            ) 
+        else:
+            inputs_embeds_vcd = None
+        if input_ids_lcd is not None:
+            (
+                input_ids_lcd,
+                position_ids,
+                _,
+                _,
+                inputs_embeds_lcd,
+                _,
+                _
+            ) = self.prepare_inputs_labels_for_multimodal(
+            # ) = self.prepare_inputs_labels_for_text(
+                input_ids_lcd,
+                position_ids,
+                None,
+                None,
+                None,
+                images,
+                image_sizes=image_sizes,
+                max_prompt_len=max_prompt_len
+            )
+        else:
+            inputs_embeds_lcd = None
+
+        if img_topk is not None:
+            sd_am = attention_mask.clone()
+            img_start = image_position[0][0]
+            img_topk = img_topk+img_start
+            sd_am[0][img_topk]=False
+        else:
+            sd_am = None
+
+        return super().generate(
+            position_ids=position_ids,
+            attention_mask=attention_mask,
+            inputs_embeds=inputs_embeds,
+            inputs_embeds_icd=inputs_embeds_icd,
+            inputs_embeds_vcd=inputs_embeds_vcd,
+            inputs_embeds_lcd=inputs_embeds_lcd,
+            image_position = image_position[0] if sid else None,
+            sd_am = sd_am if img_topk is not None else None,
+            **kwargs
+        )
+
+    def prepare_inputs_for_generation(self, input_ids, past_key_values=None,
+                                      inputs_embeds=None, **kwargs):
+        images = kwargs.pop("images", None)
+        image_sizes = kwargs.pop("image_sizes", None)
+        inputs = super().prepare_inputs_for_generation(
+            input_ids, past_key_values=past_key_values, inputs_embeds=inputs_embeds, **kwargs
+        )
+        if images is not None:
+            inputs['images'] = images
+        if image_sizes is not None:
+            inputs['image_sizes'] = image_sizes
+        return inputs
+    
+
+
+AutoConfig.register("llava_llama", LlavaConfig)
+AutoModelForCausalLM.register(LlavaConfig, LlavaLlamaForCausalLM)
---
+++ metrics.py
@@ -0,0 +1,139 @@
+import sacrebleu
+# from sacrebleu.metrics import BLEU, CHRF, TER
+from bert_score import score
+import json
+import sys
+from nltk.translate import meteor_score
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+import torch
+from pathlib import Path
+import pandas as pd
+from tqdm import tqdm
+import os
+# bert_name = "bert-base-chinese"
+bert_name = "bert-base-multilingual-cased"
+tokenizer = AutoTokenizer.from_pretrained(bert_name)
+def bleu_score(predict, answer):
+    """
+    refs = [ 
+             ['The dog bit the man.', 'It was not unexpected.', 'The man bit him first.'], 
+           ]
+    sys = ['The dog bit the man.', "It wasn't surprising.", 'The man had just bitten him.']
+    """
+    # bleu = sacrebleu.corpus_bleu(predict, answer, lowercase=True, tokenize="flores101")
+    bleu = sacrebleu.corpus_bleu(predict, answer, lowercase=True, tokenize="13a")
+    return bleu.score
+
+def chrf_score(predict, answer):
+    chrf = sacrebleu.corpus_chrf(predict, answer)
+    return chrf.score
+
+def ter_score(predict, answer):
+    ter = sacrebleu.corpus_ter(predict, answer, asian_support=True, normalized=True, no_punct=True)
+    return ter.score
+
+def bertscore(predict, answer):
+    # P, R, F1 = score(predict, answer, model_type = bert_name)
+    P, R, F1 = score(predict, answer, lang="de")
+    return torch.mean(P).item(), torch.mean(R).item(), torch.mean(F1).item()
+
+def meteor(predict, answer, type):
+    # print(answer)
+    # print(predict)
+    # print(tokenizer.tokenize(predict[0]))
+    # print(tokenizer.tokenize(answer[0]))
+    all_meteor = []
+    for i in range(len(predict)):
+        meteor = meteor_score.meteor_score(
+                            [tokenizer.tokenize(answer[i])],
+                            tokenizer.tokenize(predict[i]),
+                            )
+        all_meteor.append(meteor)
+    if type == "total":
+        return sum(all_meteor) / len(all_meteor)
+    else:
+        return all_meteor[0]
+
+def extract(data, target):
+    predicts = []
+    answers = []
+    log_probs = []
+    print("Extracting data")
+    for item in tqdm(data):
+        id = item["id"]
+        pred = item['target'].strip()
+        ans = target[id].strip()
+        # lp = item['log_sentence_prob']
+        lp = None
+        predicts.append(pred)
+        answers.append(ans)
+        log_probs.append(lp)
+    return predicts, answers, log_probs
+
+def cal_total_metrics(predicts, answers):
+    bs = bleu_score(predicts, [answers])
+    cs = chrf_score(predicts, [answers])
+    ts = ter_score(predicts, [answers])
+    p, r, f1 = bertscore(predicts, answers)
+    m = meteor(predicts, answers, "total")
+    print("BLEU:", bs)
+    print("CHRF:", cs)
+    print("TER:", ts)
+    print("BERT-P:", p, "BERT-R:", r, "BERT-F1:", f1)
+    print("METEOR:", m)
+
+    res = [{"BLEU": bs, "CHRF": cs, "TER": ts, "BERT-P": p, "BERT-R": r, "BERT-F1": f1, "METEOR": m, "CHRF<10": chrf_10}]
+    df = pd.DataFrame(res)
+    df.to_csv(file.with_name(file.stem + "_total.csv"), index=False, encoding='utf-8-sig' )
+
+def cal_each_metrics(predicts, answers):
+    # print(predicts[0])
+    # print(answers[0])
+    all_result = []
+    chrf_10 = 0
+    for i in tqdm(range(len(predicts))):
+        ans= answers[i]
+        pred = predicts[i]
+        bs = bleu_score([pred], [[ans]]) 
+        cs = chrf_score([pred], [[ans]])
+        if cs<10:
+            chrf_10+=1
+        ts = ter_score([pred], [[ans]])
+        p, r, f1 = bertscore([pred], [ans])
+        m = meteor([pred], [ans], "each")
+        # print("BLEU:", bs)
+        # print("CHRF:", cs)
+        # print("TER:", ts)
+        # print("BERT-P:", p, "BERT-R:", r, "BERT-F1:", f1)
+        # print("METEOR:", m)
+        all_result.append({"reference": ans, "predicts": pred, "BLEU": bs, "CHRF": cs, "TER": ts, 
+                           "BERT-P": p, "BERT-R": r, "BERT-F1": f1, "METEOR": m})
+    df = pd.DataFrame(all_result)
+    df.to_csv(file.with_name(file.stem + "_each.csv"), index=False, encoding='utf-8-sig')
+    print("CHRF<10:", chrf_10)
+    return chrf_10
+
+
+
+if __name__ == "__main__":
+    data_file = "/ltstorage/home/2pan/LLaVA/evaluations/multi30k/no_am/2016"
+    data_path = Path(data_file)
+    # target_file = "/ltstorage/home/2pan/dataset/3AM/data/test.zh"
+    target_file = "/ltstorage/home/2pan/dataset/multi30k/data/task1/test/test_2016_flickr.de"
+    with open(target_file, "r", encoding="utf-8") as f:
+        target = f.readlines()
+    for file in data_path.rglob("v*.json"):
+        # if os.path.exists(file.with_name(file.stem + "_total.csv")):
+        #     continue
+        print(file)
+        data = json.load(open(file, "r", encoding="utf-8"))
+        # bleu = BLEU()
+        # ter = TER()
+        predicts, answers, log_probs = extract(data, target)   
+        # with open(file.with_name(file.stem + "_log_prob.txt"), "w", encoding="utf-8") as f:
+        #     for lp in log_probs:
+        #         f.write(str(lp) + "\n")
+        print("each metrics")
+        chrf_10 = cal_each_metrics(predicts, answers)
+        print("total metrics")
+        cal_total_metrics(predicts, answers)
